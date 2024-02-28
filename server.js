003d595f730dbd334e1522880b0641ea5b6f75b0
neilmarwick <73298944+neilmarwick@users.noreply.github.com>
2024-02-28 18:48:47 +0000

    Added index.html because it got deleted somehow

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path'); // Import the 'path' module
 const app = express();
-const port = 3000;
+const port = 8081;
 
 // Define a route to handle GET requests
 /*
